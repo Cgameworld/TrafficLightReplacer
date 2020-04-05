0dbba4d0e079f3cdc2130abe2d8128cc608b5441
Cgameworld <49973689+Cgameworld@users.noreply.github.com>
2020-04-05 00:35:30 -0700

    initial xml loading works

---
+++ TrafficLightReplacer/Asset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficLightReplacer
+{
+    public class Asset
+    {
+        public string Prefab { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+    }
+}
--- TrafficLightReplacer/ModInfo.cs
+++ TrafficLightReplacer/ModInfo.cs
@@ -1,8 +1,11 @@
-﻿using ICities;
+﻿using ColossalFramework.IO;
+using ICities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 using UnityEngine;
 
 namespace TrafficLightReplacer
@@ -21,13 +24,33 @@ namespace TrafficLightReplacer
     }
     public class ModLoading : LoadingExtensionBase
     {
+        public static List<Asset> result;
+
         public override void OnLevelLoaded(LoadMode mode)
         {
-            Debug.Log("modloaded");
-            var newProp = PrefabCollection<PropInfo>.FindLoaded("1541164608.New Traffic Light 12_Data");
-            var newPropLong = PrefabCollection<PropInfo>.FindLoaded("1541164608.New Traffic Light 7_Data");  //>6 width
-            var newPropXL = PrefabCollection<PropInfo>.FindLoaded("1541164608.New Traffic Light 11_Data");  //>11 width
+            string path = Path.Combine(DataLocation.addonsPath,"test.xml");
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Asset>));
+            StreamReader reader = new StreamReader(path);
+            result = (List<Asset>)serializer.Deserialize(reader);
+            reader.Close();
 
+            for (int i = 0; i < result.Count; i++)
+            {
+                Debug.Log("entry:" + i);
+                Debug.Log("prefabname:" + result[i].Prefab);
+                Debug.Log("prefabsize:" + result[i].Size);
+            }
+            
+            ReplaceTrafficLights();
+        }
+
+        private static void ReplaceTrafficLights()
+        {
+            Debug.Log("modloaded");
+
+            var newProp = PrefabCollection<PropInfo>.FindLoaded(result[0].Prefab); 
+            var newPropLong = PrefabCollection<PropInfo>.FindLoaded(result[1].Prefab);  //>6 width
+            var newPropXL = PrefabCollection<PropInfo>.FindLoaded(result[2].Prefab);  //>11 width
 
             foreach (var prefab in Resources.FindObjectsOfTypeAll<NetInfo>())
             {
@@ -41,49 +64,26 @@ namespace TrafficLightReplacer
                 {
                     //Debug.Log("good run");
                     //var prefab = PrefabCollection<NetInfo>.FindLoaded("Medium Road");
-                  //  Debug.Log("roadloaded");
+                    //  Debug.Log("roadloaded");
 
                     float roadwidth = 0;
                     float lanecount = 0;
 
-                    //what to do about asym roads?
-                    foreach (NetInfo.Lane lane in prefab.m_lanes)
-                    {
-                        if (lane.m_laneType.ToString() == "Parking" || lane.m_laneType.ToString() == "Vehicle")
-                        {
-                            //detect one way roads - calculate width across whole road
-                            if (prefab.m_hasBackwardVehicleLanes == false || prefab.m_hasForwardVehicleLanes == false)
-                            {
-                                Debug.Log("oneway road!");
-                                Debug.Log("Lane width: " + lane.m_width + "|  Lanetype:" + lane.m_laneType);
-                                roadwidth += lane.m_width;
-                                lanecount++;
-                            }
-                            //two way roads - add widths from positive lane positions
-                            else if (lane.m_position > 0)
-                            {
-                                Debug.Log("Lane width: " + lane.m_width + "|  Lanetype:" + lane.m_laneType);
-                                roadwidth += lane.m_width;
-                                lanecount++;
-                            }
-                        }
-                    }
+                    GetRoadInformation(prefab, ref roadwidth, ref lanecount);
 
-                    Debug.Log("Total road width: " + roadwidth + " | lane count: " + lanecount) ;
-
-
+                    Debug.Log("Total road width: " + roadwidth + " | lane count: " + lanecount);
 
                     foreach (NetInfo.Lane lane in prefab.m_lanes)
                     {
                         if (lane?.m_laneProps?.m_props != null)
                         {
-                         //   Debug.Log("Lane Type:" + lane.m_laneType);
+                            //   Debug.Log("Lane Type:" + lane.m_laneType);
                             foreach (NetLaneProps.Prop propGroup in lane.m_laneProps.m_props)
                             {
                                 if (propGroup?.m_finalProp != null)
                                 {
                                     //   Debug.Log("1prop name" + propGroup.m_finalProp.name);
-                                    if (roadwidth >= 12 && lanecount>3)
+                                    if (roadwidth >= 12 && lanecount > 3)
                                     {
                                         ReplaceProp(newPropXL, propGroup);
                                     }
@@ -99,6 +99,32 @@ namespace TrafficLightReplacer
                             }
                         }
 
+                    }
+                }
+            }
+        }
+
+        private static void GetRoadInformation(NetInfo prefab, ref float roadwidth, ref float lanecount)
+        {
+            //what to do about asym roads?
+            foreach (NetInfo.Lane lane in prefab.m_lanes)
+            {
+                if (lane.m_laneType.ToString() == "Parking" || lane.m_laneType.ToString() == "Vehicle")
+                {
+                    //detect one way roads - calculate width across whole road
+                    if (prefab.m_hasBackwardVehicleLanes == false || prefab.m_hasForwardVehicleLanes == false)
+                    {
+                        Debug.Log("oneway road!");
+                        Debug.Log("Lane width: " + lane.m_width + "|  Lanetype:" + lane.m_laneType);
+                        roadwidth += lane.m_width;
+                        lanecount++;
+                    }
+                    //two way roads - add widths from positive lane positions
+                    else if (lane.m_position > 0)
+                    {
+                        Debug.Log("Lane width: " + lane.m_width + "|  Lanetype:" + lane.m_laneType);
+                        roadwidth += lane.m_width;
+                        lanecount++;
                     }
                 }
             }
