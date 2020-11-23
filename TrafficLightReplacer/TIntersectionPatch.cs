17365333976b4b01c6d47bd2557ca9627da3c328
Cgameworld <49973689+Cgameworld@users.noreply.github.com>
2020-11-23 04:50:55 -0800

    initial t-intersection prop removing test working!

---
+++ TrafficLightReplacer/TIntersectionPatch.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrafficLightReplacer
+{
+    [HarmonyPatch(typeof(NetLane))]
+    [HarmonyPatch("RenderInstance")]
+    public class TIntersectionPatch
+    {
+        public static List<uint> replaceIds = new List<uint>() { 230467, 144045 };
+        static void Prefix(uint laneID, ref NetInfo.Lane laneInfo)
+        {
+            if (replaceIds.Contains(laneID))
+            {
+                Debug.Log("DelLaneHere!!" + laneID);
+
+                NetLaneProps preProps = laneInfo.m_laneProps;
+                int len1 = preProps.m_props.Length;
+                for (int i = 0; i < len1; i++)
+                {
+                    NetLaneProps.Prop prop1 = preProps.m_props[i];
+                    if (prop1.m_prop.name == "Traffic Light Pedestrian" || prop1.m_prop.name == "Traffic Light 01")
+                    {
+                        prop1.m_finalProp = PrefabCollection<PropInfo>.FindLoaded(Tools.BlankProp);
+                    }
+                }
+            }
+        }
+    }
+}
